c464cb4173c9639f942f1bd8c5b2ea157bcafda0
Wowkiee <2240134402@qq.com>
2018-06-07 10:29:33 +0800

    HeavyChain

--- Template/Tree/HeavyChain.cpp
+++ Template/Tree/HeavyChain.cpp
@@ -1,39 +1,39 @@
 // id starts with 1
 struct HeavyChain{
-    static const int N = 100005, inf = ~0U>>1;
-    int sz[N], wson[N], top[N], dep[N], id[N], _, par[N], who[N];
-    void dfs(int c, int fa, vi g[]){
-        sz[c] = 1;
-        par[c] = fa;
-        dep[c] = dep[fa] + 1;
+	static const int N = 100005, inf = ~0U>>1;
+	int sz[N], wson[N], top[N], dep[N], id[N], _, par[N], who[N];
+	void dfs(int c, int fa, vi g[]){
+		sz[c] = 1;
+		par[c] = fa;
+		dep[c] = dep[fa] + 1;
 		int &s = wson[c] = top[c] = 0;
-        for(auto t : g[c]) if(t != fa) {
-        	dfs(t, c, g);
-        	sz[c] += sz[t];
-        	if(sz[t] >= sz[s]) s = t;
+		for(auto t : g[c]) if(t != fa) {
+			dfs(t, c, g);
+			sz[c] += sz[t];
+			if(sz[t] >= sz[s]) s = t;
 		}
-    }
-    void dfs2(int c, int fa, vi g[]){
-    	id[c] = ++_;
-    	who[_] = c;
-        int s = wson[c];
-        if(!top[c]) top[c] = c;
-        if(s) top[s] = top[c], dfs2(s, c, g);
-        for(auto t : g[c]) if(t != fa && t != s) dfs2(t, c, g);
-    }
-    void Query(int a, int b){// info in points
-        int fa = top[a], fb = top[b];
-        while(fa != fb){
-            if(dep[fa] < dep[fb]) swap(a, b), swap(fa, fb);
-            // Cal id[fa] .. id[a]
-            a = par[fa]; fa = top[a];
-        }
-        if(dep[a] < dep[b]) swap(a, b);
-        // Cal id[b] .. id[a]
-    }
-    void Build(vi g[]){
-        dfs(1, 0, g);
+	}
+	void dfs2(int c, int fa, vi g[]){
+		id[c] = ++_;
+		who[_] = c;
+		int s = wson[c];
+		if(!top[c]) top[c] = c;
+		if(s) top[s] = top[c], dfs2(s, c, g);
+		for(auto t : g[c]) if(t != fa && t != s) dfs2(t, c, g);
+	}
+	void Query(int a, int b){// info in points
+		int fa = top[a], fb = top[b];
+		while(fa != fb){
+			if(dep[fa] < dep[fb]) swap(a, b), swap(fa, fb);
+			// Cal id[fa] .. id[a]
+			a = par[fa]; fa = top[a];
+		}
+		if(dep[a] < dep[b]) swap(a, b);
+		// Cal id[b] .. id[a]
+	}
+	void Build(vi g[]){
+		dfs(1, 0, g);
 		_=0;
 		dfs2(1, 0, g);
-    }
+	}
 }hc;
