86921eceb6cbd898076c8ff282f13ee947e76ded
Wowkiee <2240134402@qq.com>
2018-08-09 20:17:54 +0800

    .vimrc

--- Template/!/.vimrc.cpp
+++ Template/!/.vimrc.cpp
@@ -1,7 +1,7 @@
-set nu ai ci si mouse=a ts=4 sts=4 sw=4
+set nu ai ci si mouse=a ts=2 sts=2 sw=2
 nmap<F2> : vs %<.in <CR>
 nmap<F3> : !gedit % <CR>
-nmap<F8> : !./%< < %<.in <CR>
+nmap<F8> : !time ./%< < %<.in <CR>
 nmap<F9> : :w <CR> :make %< <CR>
 nmap<F5> : !./%< <CR>
 nmap<F10> : :w <CR> :!g++ % -o %< -O2 -g -std=c++11 -Wall <CR>
