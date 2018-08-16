3f74c2c92e30cd0c392a4c1db5bdde37849bc646
Wowkiee <2240134402@qq.com>
2018-08-16 10:18:21 +0800

    hdu multi 8 I

--- 2018航电多校/8/i.cpp
+++ 2018航电多校/8/i.cpp
@@ -1,49 +1,124 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define rep(i, a, b) for(int i=(a); i<(b); i++)
-#define sz(a) (int)a.size()
-#define de(a) cout << #a << " = " << a << endl
-#define dd(a) cout << #a << " = " << a << " "
-#define all(a) a.begin(), a.end()
-#define endl "\n"
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-//---
-const int N = 4e5 + 7;
-const int INF=0x7fffffff;
-int r[N],sa[N],rk[N],het[N];
-int wa[N],wb[N],wx[N],wv[N];
-bool ise(int *r,int a,int b,int len) {
-	return r[a]==r[b]&&r[a+len]==r[b+len];
+const int maxn = 1e6+100;
+const int N = 1e4+100;
+const int MOD = 1e9+7;
+string s[N];
+int a[N];
+int n,m;
+int bas[maxn];
+int Ans[maxn];
+struct Suffix_Automaton{
+    int nxt[maxn*2][26],fa[maxn*2],l[maxn*2];
+    int last,cnt;
+    int ans[maxn*2];
+    int flag[maxn*2];
+    bool built[maxn*2];
+    void clear(){
+        last =cnt=1;
+        fa[1]=l[1]=0;
+        memset(nxt[1],0,sizeof nxt[1]);
+    }
+    void init(string s){
+        for (int i=0;i<s.size();i++){
+            add(s[i]-'a');
+        }
+    }
+    void add(int c){
+        int p = last;
+        int np = ++cnt;
+        l[np] = l[p]+1;
+        ans[np] =1;
+        last = np;
+        while (p&&!nxt[p][c])nxt[p][c] = np,p = fa[p];
+        if (!p)fa[np]=1;
+        else{
+            int q = nxt[p][c];
+            if (l[q]==l[p]+1)fa[np] =q;
+            else{
+                int nq = ++ cnt;
+                ans[nq]=1;
+                l[nq] = l[p]+1;
+                memcpy(nxt[nq],nxt[q],sizeof (nxt[q]));
+                fa[nq] =fa[q];
+                fa[np] = fa[q] =nq;
+                while (nxt[p][c]==q)nxt[p][c] =nq,p = fa[p];
+            }
+        }
+    }
+    void build(string s,int V,int tag){
+        int now =1;
+        for (int i=0;i<s.size();i++){
+            now = nxt[now][s[i]-'a'];
+            int temp = now;
+            while (temp!=1&&flag[temp]!=tag){
+                ans[temp] = 1LL*ans[temp]*V%MOD;
+                flag[temp] =tag;
+                temp = fa[temp];
+            }
+        }
+    }
+    void build(int now){
+        built[now]=1;
+        Ans[l[fa[now]]+1] = (Ans[l[fa[now]]+1]+ans[now])%MOD;
+        Ans[l[now]+1] = (Ans[l[now]+1]-ans[now]+MOD)%MOD;
+        for (int i=0;i<26;i++){
+            if(!nxt[now][i]||built[nxt[now][i]])continue;
+            build(nxt[now][i]);
+        }
+    }
+}sam;
+int power(int x,int y){
+  int ret =1;
+  while (y){
+    if(y&1){
+      ret = 1LL*ret*x%MOD;
+    }
+    y>>=1;
+    x = 1LL*x*x%MOD;
+  }
+  return ret;
 }
-void getSa(int n,int m) {
-	int *x=wa,*y=wb;
-	rep(i,0,m)wx[i]=0;
-	rep(i,0,n)++wx[x[i]=r[i]];
-	rep(i,1,m)wx[i]+=wx[i-1];
-	for(int i=n-1;i>=0;--i)sa[--wx[x[i]]]=i;
-	for(int j=1,p=0;p<n;j<<=1,m=p) {
-		p=0;
-		rep(i,n-j,n)y[p++]=i;
-		rep(i,0,n)sa[i]>=j?y[p++]=sa[i]-j:0;
-		rep(i,0,m)wx[i]=0;
-		rep(i,0,n)++wx[wv[i]=x[y[i]]];
-		rep(i,1,m)wx[i]+=wx[i-1];
-		for(int i=n-1;i>=0;--i)sa[--wx[wv[i]]]=y[i];
-		p=1,swap(x,y);
-		x[sa[0]]=0;
-		rep(i,1,n)x[sa[i]]=ise(y,sa[i],sa[i-1],j)?p-1:p++;
-	}
-}
-void getHeight(int n) {
-	rep(i,1,n+1)rk[sa[i]]=i;
-	for(int i=0,k=0;i<n;het[rk[i++]]=k) {
-		k=k>0?k-1:0;
-		for(int j=sa[rk[i]-1];r[i+k]==r[j+k];++k);
-	}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin>>n;
+    sam.clear();
+    for (int i=0;i<n;i++){
+        cin>>s[i];
+        sam.init(s[i]);
+        sam.last=1;
+    }
+    for (int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for (int i=0;i<n;i++){
+        sam.build(s[i],a[i],i+1);
+    }
+    sam.ans[1]=0;
+    sam.build(1);
+    for (int i=1;i<maxn;i++){
+      Ans[i] = (Ans[i-1]+Ans[i])%MOD;
+    }
+    for (int i=1;i<maxn;i++){
+      Ans[i] = (Ans[i]+Ans[i-1])%MOD;
+    }
+    bas[0]=1;
+    for (int i=1;i<maxn;i++){
+      bas[i] = 26LL*bas[i-1]%MOD;
+    }
+    for (int i=2;i<maxn;i++){
+      bas[i] = (bas[i]+bas[i-1])%MOD;
+    }
+    for (int i=1;i<maxn;i++){
+      Ans[i] = 1LL*Ans[i]*power(bas[i],MOD-2)%MOD;
+    }
+    cin>>m;
+    while (m--){
+      int x;
+      cin>>x;
+      cout<<Ans[x]<<'\n';
+    }
+    return 0;
 }
