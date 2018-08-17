4136b742515a27b701334dcfc073c0462d02ba51
Wowkiee <2240134402@qq.com>
2018-08-17 15:21:41 +0800

    cf div1 299 E

--- CF_DIV1/#299/e.cpp
+++ CF_DIV1/#299/e.cpp
@@ -18,32 +18,48 @@ typedef vector<int> vi;
 const int N = 101010;
 
 int n, q, _;
-int f[N], ans[N], sz[N], wson[N], top[N], dep[N], id[N], par[N], who[N], pre[N];
+int f[N], ans[N], sz[N], wson[N], top[N], dep[N], id[N], par[N], who[N], vis[N];
 vi g[N];
 pair<int, pii> e[N];
 pair<pii, pii> Q[N];
+
+// sum, len, llen, rlen
+pair<pii, pii> join(pair<pii, pii> A, pair<pii, pii> B) {
+	int s = A.fi.fi + B.fi.fi;
+	s -= f[A.se.se] + f[B.se.fi];
+	s += f[A.se.se + B.se.fi];
+	int ll = A.se.fi;
+	if(A.se.fi == A.fi.se) ll += B.se.fi;
+	int rl = B.se.se;
+	if(B.se.se == B.fi.se) rl += A.se.se;
+	return mp(mp(s, A.fi.se + B.fi.se), mp(ll, rl));
+}
 
 struct Seg {
 #define ls rt<<1
 #define rs ls|1
 	static const int N = ::N << 2;
-	int sum[N];
-	void upd(int p, int c, int l, int r, int rt) {
+	int sum[N], llen[N], rlen[N];
+	void upd(int p, int l, int r, int rt) {
 		if(l == r) {
-			sum[rt] = c;
+			sum[rt] = f[1];
+			vis[l] = llen[rt] = rlen[rt] = 1;
 			return ;
 		}
 		int mid = l + r >> 1;
-		if(p <= mid) upd(p, c, l, mid, ls);
-		else upd(p, c, mid+1, r, rs);
-		sum[rt] = sum[ls] + sum[rs];
+		if(p <= mid) upd(p, l, mid, ls);
+		else upd(p, mid+1, r, rs);
+		auto u = join({{sum[ls], mid-l+1}, {llen[ls], rlen[ls]}}, {{sum[rs], r-mid}, {llen[rs], rlen[rs]}});
+		sum[rt] = u.fi.fi;
+		llen[rt] = u.se.fi, rlen[rt] = u.se.se;
 	}
-	int qry(int L, int R, int l, int r, int rt) {
-		if(L <= l && r <= R) return sum[rt];
-		int mid = l + r >> 1, ans = 0;
-		if(L <= mid) ans += qry(L, R, l, mid, ls);
-		if(R >= mid+1) ans += qry(L, R, mid+1, r, rs);
-		return ans;
+	pair<pii, pii> qry(int L, int R, int l, int r, int rt) {
+		if(L > R) return {{0, 0}, {0, 0}};
+		if(L <= l && r <= R) return {{sum[rt], r-l+1}, {llen[rt], rlen[rt]}};
+		int mid = l + r >> 1;
+		if(L > mid) return qry(L, R, mid+1, r, rs);
+		if(R < mid+1) return qry(L, R, l, mid, ls);
+		return join(qry(L, R, l, mid, ls), qry(L, R, mid+1, r, rs));
 	}
 }seg;
 
@@ -52,10 +68,10 @@ void dfs(int c, int fa) {
 	par[c] = fa;
 	dep[c] = dep[fa] + 1;
 	int &s = wson[c] = top[c] = 0;
-	for(auto v : g[c]) if(v.fi != fa) {
-		dfs(v.fi, c);
-		sz[c] += sz[v.fi];
-		if(sz[v.fi] >= sz[s]) s = t;
+	for(auto t : g[c]) if(t != fa) {
+		dfs(t, c);
+		sz[c] += sz[t];
+		if(sz[t] >= sz[s]) s = t;
 	}
 }
 void dfs2(int c, int fa) {
@@ -64,30 +80,45 @@ void dfs2(int c, int fa) {
 	int s = wson[c];
 	if(!top[c]) top[c] = c;
 	if(s) top[s] = top[c], dfs2(s, c);
-	for(auto v : g[c]) if(v.fi != fa && v.fi != s) dfs2(v.fi, c);
+	for(auto t : g[c]) if(t != fa && t != s) dfs2(t, c);
 }
-
-int find(int x) {
-	if(x == pre[x]) return x;
-	return pre[x] = find(pre[x]);
-}
-void join(int x, int y) {
-	x = find(x), y = find(y);
-	if(dep[x] > dep[y]) swap(x, y);
-	pre[y] = x;
+void solve(int u, int v, int index) {
+	pair<pii, pii> r1 = {{0, 0}, {0, 0}}, r2 = r1;
+	while(top[u] != top[v]) {
+		if(dep[top[u]] > dep[top[v]]) {
+			r1 = join(seg.qry(id[top[u]]+1, id[u], 1, _, 1), r1);
+			u = top[u];
+			r1 = join({{f[vis[id[u]]], 1}, {vis[id[u]], vis[id[u]]}}, r1);
+			u = par[u];
+		} else {
+			r2 = join(seg.qry(id[top[v]]+1, id[v], 1, _, 1), r2);
+			v = top[v];
+			r2 = join({{f[vis[id[v]]], 1}, {vis[id[v]], vis[id[v]]}}, r2);
+			v = par[v];
+		}
+	}
+	if(dep[u] > dep[v]) {
+		r1 = join(seg.qry(id[v]+1, id[u], 1, _, 1), r1);
+	} else if(dep[u] < dep[v]) {
+		r2 = join(seg.qry(id[u]+1, id[v], 1, _, 1), r2);
+	}
+	swap(r1.se.fi, r1.se.se);
+	auto r = join(r1, r2);
+	ans[index] = r.fi.fi;
 }
 
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(0);
+	///read
 	cin >> n >> q;
 	rep(i, 1, n) cin >> f[i];
 	rep(i, 1, n) {
 		int u, v, w;
 		cin >> u >> v >> w;
 		e[i] = mp(w, mp(u, v));
-		g[u].pb(mp(v, w));
-		g[v].pb(mp(u, w));
+		g[u].pb(v);
+		g[v].pb(u);
 	}
 	rep(i, 1, q+1) {
 		int u, v, l;
@@ -98,8 +129,20 @@ int main() {
 	reverse(e+1, e+n);
 	sort(Q+1, Q+1+q);
 	reverse(Q+1, Q+1+q);
+	///hc
 	dfs(1, 0);
 	dfs2(1, 0);
-	rep(i, 1, n+1) pre[i] = i;
+	///solve
+	int p = 1;
+	rep(i, 1, q+1) {
+		int l = Q[i].fi.fi, u = Q[i].se.fi, v = Q[i].se.se;
+		while(p < n && e[p].fi >= l) {
+			if(dep[e[p].se.fi] > dep[e[p].se.se]) swap(e[p].se.fi, e[p].se.se);
+			seg.upd(id[e[p].se.se], 1, _, 1);
+			++p;
+		}
+		solve(u, v, Q[i].fi.se);
+	}
+	rep(i, 1, q+1) cout << ans[i] << endl;
 	return 0;
 }
