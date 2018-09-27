f7099ff401df7d43c456342907d44cc9644c3486
Wowkiee <2240134402@qq.com>
2018-09-27 15:36:45 +0800

    SuffixAutomaton.cpp

--- Template/String/6. SuffixAutomaton.cpp
+++ Template/String/6. SuffixAutomaton.cpp
@@ -1,31 +1,38 @@
- // [0,L] , 0 is virtual , 1 is rt , init!!
-struct SAM{
-    static const int N = 101010 , M = 26;
-    int par[N] , l[N] , ne[N][M];
-    int rt , last , L;
-    void add(int c){
-        int p = last , np = ++L;
-        fill(ne[np] , ne[np] + M , 0);
+/*
+ * [0,L] , 0 is virtual , 1 is rt , init!!
+ * 
+ * 一个状态 s ，由所有 right 集合是 right(s) 的字符串组成。
+ * 这些字符串的长度范围是： [l[par[s]] + 1, l[s]] 。 
+ * 状态的 right 集合是它 parent 树中所有孩子 right 集合的并集。 
+ * l[par[s]] < l[s] 
+ */ 
+struct SAM {
+    static const int N = 101010, M = 26;
+    int par[N], l[N], ne[N][M];
+    int rt, last, L;
+    void add(int c) {
+        int p = last, np = ++L;
+        fill(ne[np], ne[np] + M, 0);
         l[np] = l[p] + 1;
         last = np;
-        while(p && !ne[p][c]) ne[p][c] = np , p = par[p];
+        while(p && !ne[p][c]) ne[p][c] = np, p = par[p];
         if(!p) par[np] = rt;
         else{
             int q = ne[p][c];
             if(l[q] == l[p] + 1) par[np] = q;
-            else{
+            else {
                 int nq = ++L;
                 l[nq] = l[p] + 1;
-                copy(ne[q] , ne[q] + M , ne[nq]);
+                copy(ne[q], ne[q] + M, ne[nq]);
                 par[nq] = par[q];
                 par[q] = par[np] = nq;
-                while(p && ne[p][c] == q) ne[p][c] = nq , p = par[p];
+                while(p && ne[p][c] == q) ne[p][c] = nq, p = par[p];
             }
         }
     }
-    void ini(){
+    void ini() {
         rt = last = L = 1;
-        fill(ne[rt] , ne[rt] + M , 0);
+        fill(ne[rt], ne[rt] + M, 0);
         l[0] = -1;
     }
 };
