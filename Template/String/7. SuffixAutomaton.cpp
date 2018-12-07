c73d2b7c433b9043c3da5c56d056d8df658b7cd1
Wowkiee <2240134402@qq.com>
2018-12-07 20:54:48 +0800

    SAM

--- Template/String/7. SuffixAutomaton.cpp
+++ Template/String/7. SuffixAutomaton.cpp
@@ -1,11 +1,7 @@
 /*
  * [0,L] , 0 is virtual , 1 is rt , init!!
- * 
- * parent 树和 trans 都是 DAG 。 
- * 一个状态 s ，由所有 right 集合是 right(s) 的字符串组成。
- * 这些字符串的长度范围是： [l[par[s]] + 1, l[s]] 。 
- * 状态的 right 集合是它 parent 树中所有孩子 right 集合的并集。 
- * l[par[s]] < l[s] 
+ * [l[par[s]] + 1, l[s]] 
+ * 好像暴力向上跳的复杂度是对的。
  */ 
 struct SAM {
     static const int N = ::N << 1, M = 26;
@@ -13,7 +9,7 @@ struct SAM {
     int rt, last, L;
     void add(int c) {
         int p = last;
-        /* 广义后缀自动机 
+        /* ex
 		if(ne[p][c] && l[ne[p][c]] == l[p] + 1) {
 			last = ne[p][c];
 			return ;
@@ -43,23 +39,8 @@ struct SAM {
         fill(ne[rt], ne[rt] + M, 0);
         l[0] = -1;
     }
-	// 一开始拿 s 建的自动机，这部分仅用于参考，不是板子的一部分。
-	string s;
-	int right[N], cc[N], cur[N];
-	void build() {
-		fill_n(right, L + 1, 0);
-		fill_n(cc, L + 1, 0);
-		int p = rt;
-		for(auto u : s) {
-			p = ne[p][u - 'a'];
-			++right[p];
-		}
-		rep(i, 1, L + 1) ++cc[l[i]];
-		rep(i, 1, L + 1) cc[i] += cc[i - 1];
-		rep(i, 1, L + 1) cur[cc[l[i]]--] = i;
-		for(int i = L; i >= 2; --i) {
-			int u = cur[i];
-			right[par[u]] += right[u];
-		}
-	}
 };
+// BucketSort
+rep(i, 1, L + 1) ++cnt[l[i]];
+rep(i, 1, L + 1) cnt[i] += cnt[i - 1];
+rep(i, 1, L + 1) cur[cnt[l[i]]--] = i;
